e78109b136e4d559966182b8b647eda13151332f
arnobodrib <arnobodrib@gmail.com>
2019-10-16 23:44:17 +0600

    Assignment 11

---
+++ RefrigeratorAppPractice3/RefrigeratorUi.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace RefrigeratorAppPractice3
+{
+    public partial class RefrigeratorUi : Form
+    {
+        public RefrigeratorUi()
+        {
+            InitializeComponent();
+        }
+
+        public Weight weight;
+        public double Add;
+        public string Current;
+        public string Remain;
+
+        
+
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            weight = new Weight();
+            weight.MaxWeight = Convert.ToInt32(maxWeightTakeTextBox.Text);
+        }
+
+        private void EnterButton_Click(object sender, EventArgs e)
+        {
+            weight.AddItemWeight(Convert.ToInt32(itemTextBox.Text), Convert.ToDouble(weightTextBox.Text));
+            currentWeightTextBox.Text = (weight.CurrentWeight(Convert.ToInt32(itemTextBox.Text), Convert.ToDouble(weightTextBox.Text))).ToString();
+            remainingWeightTextBox.Text = (weight.RemainWeight(Convert.ToInt32(maxWeightTakeTextBox.Text))).ToString();
+
+            MessageBox.Show("Done");
+        }
+    }
+}
---
+++ RefrigeratorAppPractice3/Weight.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefrigeratorAppPractice3
+{
+    
+
+
+    public class Weight
+    {
+        public int MaxWeight { get; set; }
+        public List<int> ItemNo = new List<int>();
+        public List<Double> WeightUnit = new List<double>();
+        public double CurrentW=0;
+        public double RemainW;
+
+        public void AddItemWeight(int i, double w)
+        {
+            ItemNo.Add(i);
+            WeightUnit.Add(w);
+
+        }
+
+        public double CurrentWeight(int i, double w)
+        {
+            CurrentW = CurrentW + Convert.ToDouble(i) * w;
+            return CurrentW;
+
+        }
+
+        public double RemainWeight(int m)
+        {
+            RemainW = m - CurrentW;
+            return RemainW;
+        }
+
+
+
+    }
+
+    
+
+
+}
